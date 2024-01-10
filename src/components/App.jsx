fbf8702173f4ee68e4dc2c9d731d12a4db4e2c46
YevhenBez <byxgalter@bigmir.net>
2024-01-10 18:24:01 +0200

    Сделал компонент   Customers  и применил маршруты. Тестовое для бота  Front-end.work  в  Телеграмме.
    Сделал компонент   Customers  и применил маршруты. Для этого в файле  Customers.jsx   написал функцию   Customers  в которой вернул немного разметки и затем экспортнул её. Затем в файле   index.js   (который находится по пути   src\pages\customers   ) сделал реэкспорт компонента   Customers   , чтобы сократить длинну пути при вызове компонента   Customers. Затем в файле  App.jsx   импортнул    Routes, Route   для маршрутов и   lazy   для ленивой загрузки. Затем при помощи   lazy   импортнул компонент   Customers   Затем в функции   App   разрендерил компонент   Customers   используя маршруты. И затем экспортнул функцию    App   Затем в файле    index.js   (который находится в папке    src   ) при импорте    App    я убрал скобки, т.к. в файле   App.jsx   я использовал другой вид экспорта   App  Тестовое для бота  Front-end.work  в  Телеграмме.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
+import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
+const Customers = lazy(() => import('../pages/customers'));
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Customers />}/>
+      </Routes>
     </div>
   );
 };
+
+export default App;
---
+++ src/pages/customers/Customers.jsx
@@ -0,0 +1,22 @@
+
+
+const Customers = () => {
+  
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+        <h1>Здесь будут контакты клиентов</h1>
+      
+    </div>
+  );
+};
+
+export default Customers;
