69f215e87bc9703ab9ecf611737127bab23c0ee0
YevhenBez <byxgalter@bigmir.net>
2024-01-15 00:25:00 +0200

    Добавил в   img   спрайт   sprite-icon.svg    для    svg-иконок и оптимизировал его. Тестовое для бота Front-end.work в Телеграмме.
    Добавил в   img   спрайт   sprite-icon.svg    для    svg-иконок и оптимизировал его. Для этого с макета в   figma   я выгрузил необходимую иконку в формате   svg   Затем используя сайт   https://icomoon.io/app/#/select/image     я сделал спрайт для   svg-иконок. А именно - на сайте  https://icomoon.io/app/#/select/image   в менюшке я выбрал сначала   "Clear Selection"   , чтобы очистить выбранные ранее иконки-svg   Затем, когда вы увидите что интерфейс данного сайта отображает вам "SVGs: 0....."   , то в тексте  "Nothing to show! Please select some icons first"   (который находится под данным    "SVGs: 0....."   ) выберите интерактивное   select    В появившемся интерфейсе выбери   "Import Icons"    и выбери необходимую иконку-svg    которую ты ранее выгрузил с макета в    figma    В появившемся интерфейсе выбери иконку-svg   , которую ты загрузил на данный сайт, и нажми "Generate SVG & More"   и после этого нажми кнопку   Download    При этом к тебе на комп загрузится архив с именем   icomoon   В данном архиве бери всего один файл, а именно    symbol-defs.svg   и вставляй его в необходимую папку проекта - это и есть СПРАЙТ (в данном проекте данный спрайт я вставил в папку    svg   и переименовал его на    sprite-icon.svg   ). Также у необходимой мне   svg-иконки в данном спрайте я изменил   id   на   hexagon-for-Dashboard   (мне так просто захотелось). Также данный спрайт можно ещё оптимизировать используя обычный компрессор (чтобы просто сжать его, уменьшив размер данного Спрайта). В данном тестовом я оптимизировал Спрайт при помощи    https://svgomg.net/   (просто закидываешь в него Спрайт, а выгружаешь с него уже оптимизированный). Затем в файле     Sidebar.jsx    я импортнул    Link   из     react-router-dom    чтобы при помощи него переходить на нужный мне маршрут при клацании на содержимом, которое он обернул. Также импортнул сам Спрайт и в строке 10   данного файла я получаю необходимую мне   svg-иконку. Тестовое для бота Front-end.work в Телеграмме.

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,17 @@
 import Navigation from '../../components/navigation';
+import { Link } from 'react-router-dom';
+import sprite from '../../img/svg/sprite-icon.svg';
 
 const Sidebar = () => {
   return (
     <div >
+        <Link to="/" >
+          <svg width="37" height="37" >
+            <use xlinkHref={`${sprite}#hexagon-for-Dashboard`} />
+          </svg>
+            <p >Dashboard</p>
+            <p>v.01</p>
+        </Link>
       <Navigation />
     </div>
   );
