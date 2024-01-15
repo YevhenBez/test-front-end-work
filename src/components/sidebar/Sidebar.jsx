324f2ec8bcb9617d8ce7bf55cf7ebae9717920da
YevhenBez <byxgalter@bigmir.net>
2024-01-15 02:16:41 +0200

    Стилизировал логотип. Тестовое для бота Front-end.work в Телеграмме.
    Стилизировал логотип. Для этого в файл   Sidebar.jsx   импортнул стили, которые я прописал в файле    sidebar.module.css   Тестовое для бота Front-end.work в Телеграмме.

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,16 +1,19 @@
 import Navigation from '../../components/navigation';
 import { Link } from 'react-router-dom';
 import sprite from '../../img/svg/sprite-icon.svg';
+import css from '../../components/sidebar/css/sidebar.module.css';
 
 const Sidebar = () => {
   return (
     <div >
-        <Link to="/" >
+        <Link to="/" className={css.logo}>
           <svg width="37" height="37" >
             <use xlinkHref={`${sprite}#hexagon-for-Dashboard`} />
           </svg>
-            <p >Dashboard</p>
-            <p>v.01</p>
+            <div className={css.logo__name}>
+                <p className={css.logo__name__text} >Dashboard</p>
+                <p className={css.logo__name__version}>v.01</p>
+            </div>
         </Link>
       <Navigation />
     </div>
