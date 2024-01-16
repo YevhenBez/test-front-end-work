440aa7153306ca91886799d27835742ab83257bd
YevhenBez <byxgalter@bigmir.net>
2024-01-17 01:21:43 +0200

    Переделал название стилей под БЭМ. Тестовое для бота Front-end.work в Телеграмме.
    Переделал название стилей под БЭМ. Тестовое для бота Front-end.work в Телеграмме.

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -7,9 +7,9 @@ const Navigation = () => {
     <nav>
       <ul className={css.ul}>
         <li className={css.ul__li}>
-          <NavLink to="/other" className={css.ul__navLink}>
-            <button type="button" className={css.ul__navLink__btn}>
-              <svg width="24" height="24" className={css.ul__navLink__btn__bgnSvg}>
+          <NavLink to="/other" className={css.ul__li__navLink}>
+            <button type="button" className={css.ul__li__navLink__btn}>
+              <svg width="24" height="24" className={css.ul__li__navLink__btn__bgnSvg}>
                 <use xlinkHref={`${sprite}#key-square`} />
               </svg>
               Dashboard
@@ -17,9 +17,9 @@ const Navigation = () => {
           </NavLink>
         </li>
         <li className={css.ul__li}>
-          <NavLink to="/other" className={css.ul__navLink}>
-            <button type="button" className={css.ul__navLink__btn}>
-              <svg width="24" height="24" className={css.ul__navLink__btn__bgnSvg}>
+          <NavLink to="/other" className={css.ul__li__navLink}>
+            <button type="button" className={css.ul__li__navLink__btn}>
+              <svg width="24" height="24" className={css.ul__li__navLink__btn__bgnSvg}>
                 <use xlinkHref={`${sprite}#volumetric-square`} />
               </svg>
               Product
@@ -27,23 +27,23 @@ const Navigation = () => {
           </NavLink>
         </li>
         <li className={css.ul__li}>
-          <NavLink to="/" className={css.ul__navLink}>
-            <button type="button" className={css.ul__navLink__btn}>Customers</button>
+          <NavLink to="/" className={css.ul__li__navLink}>
+            <button type="button" className={css.ul__li__navLink__btn}>Customers</button>
           </NavLink>
         </li>
         <li className={css.ul__li}>
-          <NavLink to="/other" className={css.ul__navLink}>
-            <button type="button" className={css.ul__navLink__btn}>Income</button>
+          <NavLink to="/other" className={css.ul__li__navLink}>
+            <button type="button" className={css.ul__li__navLink__btn}>Income</button>
           </NavLink>
         </li>
         <li className={css.ul__li}>
-          <NavLink to="/other" className={css.ul__navLink}>
-            <button type="button" className={css.ul__navLink__btn}>Promote</button>
+          <NavLink to="/other" className={css.ul__li__navLink}>
+            <button type="button" className={css.ul__li__navLink__btn}>Promote</button>
           </NavLink>
         </li>
         <li>
-          <NavLink to="/other" className={css.ul__navLink}>
-            <button type="button" className={css.ul__navLink__btn}>Help</button>
+          <NavLink to="/other" className={css.ul__li__navLink}>
+            <button type="button" className={css.ul__li__navLink__btn}>Help</button>
           </NavLink>
         </li>      
       </ul>
