a4d531348d01727764e97d704351cdbcadfac586
YevhenBez <byxgalter@bigmir.net>
2024-01-26 00:09:20 +0200

    Сделал таблицу на страничке   Customers   и стилизировал её. Тестовое для бота Front-end.work в Телеграмме.
    Сделал таблицу на страничке   Customers   и стилизировал её. Для этого сделал файл  customers.json   с данными таблицы. Затем импортнул данный   customers.json   в файл   Customers.jsx   и в функции   Customers   вернул разметку таблицы. При этом обращаю внимание, чтобы шапка данной таблицы была подчёркнута полностью, а подчёркивание строк данной таблицы начиналось после некоторого отступа, я сделал в шапке данной таблицы пустые  <th></th>   и стилизировал их соответственно ТЗ данного тестового и также в теле таблицы сделал пустые    <td></td>    (но стилизировать   td   уже не нужно). Чтобы стилизировать ячейки в столбике  Status   я обернул в   div   значения, которые я вывожу в данном столбике. И стилизировал затем данный   div   (см. строку кода 30  ) Это я сделал для того, что в таблице не предусмотрена стилизация ячейки (т.е. стилизировать ячейку при помощи кода невозможно).  Сами стили для данной таблицы прописал в файле  customers.module.css   Тестовое для бота Front-end.work в Телеграмме.

--- src/pages/customers/Customers.jsx
+++ src/pages/customers/Customers.jsx
@@ -1,20 +1,38 @@
-
+import listCustomers from '../../path/to/customers.json';
+import css from '../../pages/customers/css/customers.module.css';
 
 const Customers = () => {
   
   return (
-    <div
-      style={{
-        height: '100vh',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-        <h1>Здесь будут контакты клиентов</h1>
-      
-    </div>
+    <table className={css.customersTable}>
+            <thead>
+            <tr className={css.customersTable__tr}>
+                <th className={css.customersTable__tr__indentBgn}></th>
+                <th className={css.customersTable__tr__th}>Customer Name</th>
+                <th className={css.customersTable__tr__th}>Company</th>
+                <th className={css.customersTable__tr__th}>Phone Number</th>
+                <th className={css.customersTable__tr__th}>Email</th>
+                <th className={css.customersTable__tr__th}>Country</th>
+                <th className={css.customersTable__tr__thStatus}>Status</th>
+                <th className={css.customersTable__tr__indentEnd}></th>
+            </tr>
+            </thead>
+
+            <tbody>
+                {listCustomers.map(listCustomer => (
+                        <tr key={listCustomer.id}>
+                            <td></td>
+                            <td className={css.customersTable__td}>{listCustomer.name}</td>
+                            <td className={css.customersTable__td}>{listCustomer.company}</td>
+                            <td className={css.customersTable__td}>{listCustomer.phone}</td>
+                            <td className={css.customersTable__td}>{listCustomer.email}</td>
+                            <td className={css.customersTable__td}>{listCustomer.country}</td>
+                            <td className={css.customersTable__tdStatus}><div className={listCustomer.status === "Inactive" ? css.customersTable__tdStatus__inactive : css.customersTable__tdStatus__active}>{listCustomer.status}</div></td>
+                            <td></td>
+                        </tr>
+                    ))}
+            </tbody>
+        </table>
   );
 };
 
